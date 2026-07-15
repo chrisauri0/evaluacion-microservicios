db8a16d317a6fb5719179085d3ef3c49b907da93
chrisauri0 <jonathancz1912@gmail.com>
2026-07-14 23:55:05 -0600

    como hacer peticiones en angular al apigateway

--- api-gateway/src/main/java/com/example/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/example/api_gateway/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.api_gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -18,7 +19,9 @@ public class SecurityConfig {
             .authorizeExchange(exchanges -> exchanges
                 .pathMatchers(org.springframework.http.HttpMethod.POST, "/api/usuarios").permitAll()
                 .pathMatchers("/oauth2/**", "/login/**").permitAll()
+                 .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll() // <-- clave
                 .anyExchange().authenticated()
+                
             )
             .oauth2ResourceServer(oauth2 -> oauth2
                 .jwt(Customizer.withDefaults())
